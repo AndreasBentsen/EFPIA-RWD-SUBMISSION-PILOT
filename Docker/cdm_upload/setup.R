caec7594a1face70e52132b343f4a07c6e1a228b
Christian Kruse <christian.kruse.bt@gmail.com>
2021-11-15 18:15:49 +0100

    docker image use CdmUpload.R to upload csv to postgres docker

---
+++ Docker/cdm_upload/setup.R
@@ -0,0 +1,4 @@
+if (!require("rJava")) install.packages("rJava")
+if (!require("DatabaseConnector")) install.packages("DatabaseConnector")
+if (!require("readr")) install.packages("readr")
+if (!require("SqlRender")) install.packages("SqlRender")
