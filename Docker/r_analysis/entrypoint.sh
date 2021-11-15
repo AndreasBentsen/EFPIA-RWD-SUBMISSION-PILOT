b6989c825a9a77c1d941755dac8b511470c1873e
Christian Kruse <christian.kruse.bt@gmail.com>
2021-11-15 18:16:06 +0100

    docker r analysis script for covid 19 hydroxy study

---
+++ Docker/r_analysis/entrypoint.sh
@@ -0,0 +1,5 @@
+#cp /EFPIA-RWD-SUBMISSION-PILOT/Docker/cdm_upload_rstudio/.Renviron /root/.Renviron
+
+# Set up packages and keyring
+#Rscript "/EFPIA-RWD-SUBMISSION-PILOT/Docker/cdm_upload_rstudio/setup.R"
+#Rscript "/EFPIA-RWD-SUBMISSION-PILOT/Docker/cdm_upload_rstudio/UploadTables.R"
