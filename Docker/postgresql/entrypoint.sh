5d39b7ed5515d9b3f4a14c8576d34760746de5a6
Christian Kruse <christian.kruse.bt@gmail.com>
2021-11-15 18:13:59 +0100

    postgresql docker

---
+++ Docker/postgresql/entrypoint.sh
@@ -0,0 +1,1 @@
+echo "Hej postgresql"
---
+++ Docker/postgresql/tune_db.sql
@@ -0,0 +1,12 @@
+ALTER SYSTEM SET max_connections = '40';
+ALTER SYSTEM SET shared_buffers = '8GB';
+ALTER SYSTEM SET effective_cache_size = '24GB';
+ALTER SYSTEM SET maintenance_work_mem = '2GB';
+ALTER SYSTEM SET checkpoint_completion_target = '0.9';
+ALTER SYSTEM SET wal_buffers = '16MB';
+ALTER SYSTEM SET default_statistics_target = '500';
+ALTER SYSTEM SET random_page_cost = '1.1';
+ALTER SYSTEM SET effective_io_concurrency = '200';
+ALTER SYSTEM SET work_mem = '52428kB';
+ALTER SYSTEM SET min_wal_size = '4GB';
+ALTER SYSTEM SET max_wal_size = '16GB';
