6489294c0ad66dc6f0370f749d530272b5fd5e44
Schuemie <MSCHUEMI@its.jnj.com>
2022-03-29 13:44:35 +0200

    Comparing propensity model regression in and output between sites

---
+++ publication/propensity_regression/CompareRegressions.R
@@ -0,0 +1,158 @@
+library(CohortMethod)
+library(dplyr)
+
+cmData1 <- loadCohortMethodData("JnJ_ThreadFix/CmData_l1_t137_c143.zip")
+cmData2 <- loadCohortMethodData("NN_ThreadFix/CmData_l1_t137_c143.zip")
+
+
+all.equal(
+  cmData1$cohorts %>%
+    arrange(rowId) %>%
+    collect(),
+  cmData2$cohorts %>%
+    arrange(rowId) %>%
+    collect()
+)
+# TRUE
+
+all.equal(
+  cmData1$covariates %>%
+    arrange(rowId, covariateId) %>%
+    collect(),
+  cmData2$covariates %>%
+    arrange(rowId, covariateId) %>%
+    collect()
+)
+# TRUE
+
+all.equal(
+  cmData1$outcomes %>%
+    arrange(rowId, outcomeId) %>%
+    collect(),
+  cmData2$outcomes %>%
+    arrange(rowId, outcomeId) %>%
+    collect()
+)
+# TRUE
+
+ps1 <- readRDS("JnJ_ThreadFix/Ps_l1_s2_p1_t137_c143.rds")
+ps2 <- readRDS("NN_ThreadFix/Ps_l1_s2_p1_t137_c143.rds")
+
+merged <- inner_join(
+  ps1 %>%
+    select(rowId, ps1 = propensityScore),
+  ps2 %>%
+    select(rowId, ps2 = propensityScore),
+  by = "rowId"
+)
+
+
+different <- merged %>%
+  filter(ps1 != ps2)
+
+different %>%
+  mutate(delta = ps1 - ps2) %>%
+  summarise(max(delta))
+# # A tibble: 1 x 1
+# `max(delta)`
+# <dbl>
+#   1     9.99e-16
+
+
+matched1 <- matchOnPs(ps1)
+matched2 <- matchOnPs(ps2)
+
+mergedMatched <- inner_join(
+  matched1 %>%
+    select(rowId, stratumId1 = stratumId),
+  matched2 %>%
+    select(rowId, stratumId2 = stratumId),
+  by = "rowId"
+)
+sum(mergedMatched$stratumId1 != mergedMatched$stratumId2)
+# 21
+
+.Machine
+# $double.eps
+# [1] 2.220446e-16
+# 
+# $double.neg.eps
+# [1] 1.110223e-16
+# 
+# $double.xmin
+# [1] 2.225074e-308
+# 
+# $double.xmax
+# [1] 1.797693e+308
+# 
+# $double.base
+# [1] 2
+# 
+# $double.digits
+# [1] 53
+# 
+# $double.rounding
+# [1] 5
+# 
+# $double.guard
+# [1] 0
+# 
+# $double.ulp.digits
+# [1] -52
+# 
+# $double.neg.ulp.digits
+# [1] -53
+# 
+# $double.exponent
+# [1] 11
+# 
+# $double.min.exp
+# [1] -1022
+# 
+# $double.max.exp
+# [1] 1024
+# 
+# $integer.max
+# [1] 2147483647
+# 
+# $sizeof.long
+# [1] 4
+# 
+# $sizeof.longlong
+# [1] 8
+# 
+# $sizeof.longdouble
+# [1] 16
+# 
+# $sizeof.pointer
+# [1] 8
+# 
+# $longdouble.eps
+# [1] 1.084202e-19
+# 
+# $longdouble.neg.eps
+# [1] 5.421011e-20
+# 
+# $longdouble.digits
+# [1] 64
+# 
+# $longdouble.rounding
+# [1] 5
+# 
+# $longdouble.guard
+# [1] 0
+# 
+# $longdouble.ulp.digits
+# [1] -63
+# 
+# $longdouble.neg.ulp.digits
+# [1] -64
+# 
+# $longdouble.exponent
+# [1] 15
+# 
+# $longdouble.min.exp
+# [1] -16382
+# 
+# $longdouble.max.exp
+# [1] 16384
